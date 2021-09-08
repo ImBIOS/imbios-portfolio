ff58f1a7c3efef182f3735c31f50ad493ec7d3ba
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2021-09-09 06:52:49 +0700

    first commit

---
+++ constants/data.js
@@ -0,0 +1,102 @@
+const userData = {
+  githubUsername: "ImBIOS",
+  name: "Imamuzzaki Abu Salam",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.jpeg",
+  email: "imamuzzaki@gmail.com",
+  phone: "+62 85156451441",
+  address: "Bogor, West Java, Indonesia.",
+  projects: [
+    {
+      title: "Juara Koding",
+      link: "https://juarakoding.com",
+      imgUrl: "/jk.png",
+    },
+    {
+      title: "Hujian",
+      link: "https://imbios.github.io/Hujian",
+      imgUrl: "/hujian.png",
+    },
+    {
+      title: "Soto Malam",
+      link: "https://imbios.github.io/soto-malam-mfwde",
+      imgUrl: "/soto-malam.png",
+    },
+    {
+      title: "Election App",
+      link: "https://maktab-election-deprecated.vercel.app/",
+      imgUrl: "/election.png",
+    },
+    {
+      title: "Gubuk Imam",
+      link: "https://gubuk-imam.vercel.app/",
+      imgUrl: "/gubuk-imam.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing frontend web application since I was 15 years old. I didn't know what frontend meant at that time because the term was not coined back then.`,
+      `After learning HTML and struggling with CSS and Javascript, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `I've been coding in React, Node.js, Express, PostgreSQL, MySQL, Bootstrap, Tailwind and C# programming language. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer. (LOL)`,
+    ],
+    currentProject: "Juara Koding",
+    currentProjectUrl: "https://juarakoding.com",
+  },
+  experience: [
+    {
+      title: "Webinar Speaker",
+      company: "Dicoding | Cloud and Backend",
+      year: "June 2021",
+      companyLink: "https://juarakoding.com",
+      desc: "I got an oportunity to speak in front of Educators (Vocational Teachers and Lecturers) to talk about AWS Cloud and Backend.",
+    },
+    {
+      title: "Frontend Developer",
+      company: "Juara Koding",
+      year: "Apr 2021",
+      companyLink: "https://juarakoding.com",
+      desc: "I love to share my knowledge and finding with people. It is the way I make impact to people.",
+    },
+    {
+      title: "School Android App Province Champion",
+      company: "Pustekkom | Coding Edusiber",
+      year: "Nov 2019",
+      companyLink: "https://juarakoding.com",
+      desc: "I code an app and win.",
+    },
+    {
+      title: "Chairman of Student Council",
+      company: "OSIS SMAN 1 Gemolong, Sragen.",
+      year: "Oct 2019",
+      companyLink: "https://sman1gemolong.sch.id",
+      desc: "Being a volunteer in my scool student council. Fun and learn a lot.",
+    },
+    {
+      title: "Freelance",
+      company: "Remote",
+      year: "Aug 2019",
+      companyLink: "https://sman1gemolong.sch.id",
+      desc: "Owl at night. Web, Game, Flash, Android, almost everything, right?",
+    },
+    {
+      title: "Chairman of Student Council",
+      company: "IPM SMP Al-Qolam Muh. Gemolong, Sragen.",
+      year: "Sep 2017",
+      companyLink: "https://sman1gemolong.sch.id",
+      desc: "IPM stands for Ikatan Pelajar Muhammadiyah, and that's it.",
+    },
+  ],
+  resumeUrl:
+    "https://excampur-my.sharepoint.com/:b:/g/personal/imbios_excampur_onmicrosoft_com/EV_WYLYqy01FinyMfxe1i3cBSmeqVsjRpcJ9e7WZhggUNg?e=pF9x4c",
+  socialLinks: {
+    instagram: "https://instagram.com/abusalam16",
+    twitter: "https://twitter.com/ImamuzzakiS",
+    linkedin: "https://linkedin.com/in/imamuzzaki-abu-salam",
+    github: "https://github.com/ImBIOS",
+    facebook: "https://facebook.com/imamuzzaki.abusalam",
+  },
+};
+
+export default userData;
