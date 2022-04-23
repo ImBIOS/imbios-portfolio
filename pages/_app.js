fb4e638cb28b9d2ff1c5a05a468a0e45fac97e35
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-04-23 18:57:08 +0700

    add umami micin.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,15 @@ import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <Script
+        async
+        defer
+        data-website-id="14dc6d24-94c8-4de7-80e4-e34df1a2c5d6"
+        src="https://micin.imam.dev/micin.js"
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
